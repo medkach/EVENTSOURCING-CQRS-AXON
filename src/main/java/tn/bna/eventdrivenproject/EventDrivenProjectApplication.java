a1d264f22895f1a1356c9aef7a29c20cf74deafb
medkach <mohamed.kachout1@gmail.com>
2022-03-03 09:54:52 +0100

    EVENT DRIVEN PROJECT WITH AXON-QI FRAMEWOR

---
+++ src/main/java/tn/bna/eventdrivenproject/EventDrivenProjectApplication.java
@@ -0,0 +1,13 @@
+package tn.bna.eventdrivenproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventDrivenProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EventDrivenProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/bna/eventdrivenproject/EventDrivenProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.bna.eventdrivenproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventDrivenProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
